0d642d3e304281e75f0ecd5b5702963679d390da
amoodie <amoodie@rice.edu>
2020-09-14 21:03:03 -0500

    add an initial version of a shoreline roughness calculation

--- deltametrics/plan.py
+++ deltametrics/plan.py
@@ -2,6 +2,69 @@ import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 
+from . import mask
+
+
+def compute_shoreline_roughness(shore_mask, land_mask):
+    """Compute shoreline roughness.
+
+    Computes the shoreline roughness metric:
+
+    .. math::
+
+        L_{shore} / \sqrt{A_{land}}
+
+    given binary masks of the shoreline and land area.
+
+    Parameters
+    ----------
+    shore_mask : :obj:`~deltametrics.mask.ShorelineMask`, :obj:`ndarray`
+        Shoreline mask. Can be a :obj:`~deltametrics.mask.ShorelineMask` object,
+        or a binarized array.
+
+    land_mask : :obj:`~deltametrics.mask.LandMask`, :obj:`ndarray`
+        Land mask. Can be a :obj:`~deltametrics.mask.LandMask` object,
+        or a binarized array.
+
+    Returns
+    -------
+    roughness : :obj:`float`
+        Shoreline roughness, computed as described above.
+    """
+
+    if isinstance(shore_mask, mask.ShorelineMask):
+        _sm = shore_mask.mask
+    if isinstance(land_mask, mask.LandMask):
+        _lm = land_mask.mask
+
+    if np.sum(_sm) > 0:
+        # sort the shoreline mask into a shoreline "line"
+        _y, _x = np.argwhere(_sm).T
+        _closest = np.argmin(np.sqrt((_x-0)**2 + (_y-0)**2))
+        _xs = np.zeros(len(_x))
+        _ys = np.zeros(len(_y))
+        _xs[0] = _x[_closest]
+        _ys[0] = _y[_closest]
+        _hit = np.zeros(len(_x), dtype=np.bool)
+        _hit[_closest] = True
+        for i in range(len(_x)-1):
+            _xi = _xs[i]
+            _yi = _ys[i]
+            _dists = np.sqrt((_x[~_hit]-_xi)**2 + (_y[~_hit]-_yi)**2)
+            _whr = np.argmin(_dists)
+            _xs[i+1] = _x[~_hit][_whr]
+            _ys[i+1] = _y[~_hit][_whr]
+            __whr = np.argwhere(~_hit)
+            _hit[__whr[_whr]] = True
+
+        shore_len_pix = np.sum(np.sqrt((_xs[1:]-_xs[:-1])**2 +
+                                       (_ys[1:]-_ys[:-1])**2))
+        land_area_pix = np.sum(_lm)
+        rough = shore_len_pix / np.sqrt(land_area_pix)
+    else:
+        rough = np.nan
+    return rough
+
 
 def a_land_function(mask):
     """Compute a land-water function
