04c79c91f41c39efa001e6ba57d2c49271ab6e3f
jayh <jayaram.hariharan@utexas.edu>
2021-08-16 16:29:18 -0400

    three dots missing in example doc-string

--- deltametrics/plan.py
+++ deltametrics/plan.py
@@ -453,7 +453,7 @@ class MorphologicalPlanform(BasePlanform):
             >>> MP = dm.plan.MorphologicalPlanform.from_elevation_data(
             ...     golfcube['eta'][-1, :, :],
             ...     elevation_threshold=0,
-                    max_disk=3)
+            ...     max_disk=3)
         """
         # make a temporary mask
         _em = mask.ElevationMask(
